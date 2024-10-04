ad5ac685c7cac582b9b4531af9c1df616b4f39d5
Piotr Gliszczyński <pgliszczynski@adp.com>
2024-10-04 17:28:32 +0200

    feat/#9 - add Customer domain object and DTO

---
+++ data-service/src/main/java/com/training/domain/CustomerDto.java
@@ -0,0 +1,17 @@
+package com.training.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerDto {
+
+  private Integer id;
+  private String email;
+  private String name;
+  private String password;
+  
+}
