a27b8792baa08afdecaf23f9c4980b63024de914
Piotr Gliszczyński <pgliszczynski@adp.com>
2024-10-07 13:13:00 +0200

    feat/#13 - add Handling Duplicate elements exception

--- data-service/src/main/java/com/training/config/GlobalHttpExceptionHandler.java
+++ data-service/src/main/java/com/training/config/GlobalHttpExceptionHandler.java
@@ -1,15 +1,23 @@
 package com.training.config;
 
+import com.training.exception.DuplicateElementsException;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class GlobalHttpExceptionHandler extends ResponseEntityExceptionHandler {
+
+  @ExceptionHandler(DuplicateElementsException.class)
+  public ResponseEntity<Object> handleDuplicateElementsException(DuplicateElementsException ex) {
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+  }
 
   @Override
   protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
