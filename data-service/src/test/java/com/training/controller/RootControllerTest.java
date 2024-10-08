d8807b279d11b01ee1c8e7d22a577c2a117c89cf
Piotr Gliszczyński <pgliszczynski@adp.com>
2024-10-08 11:00:19 +0200

    feat/#42 - add security to data-service and update tests

--- data-service/src/test/java/com/training/controller/RootControllerTest.java
+++ data-service/src/test/java/com/training/controller/RootControllerTest.java
@@ -1,28 +1,44 @@
 package com.training.controller;
 
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
-@SpringJUnitWebConfig
-@WebMvcTest(RootController.class)
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration
+@SpringBootTest
 class RootControllerTest {
 
-  @Autowired
   private MockMvc mockMvc;
+  @Autowired
+  private WebApplicationContext context;
 
   private static List<HttpMethod> provideDisallowedHttpMethods() {
     return List.of(HttpMethod.POST, HttpMethod.PUT, HttpMethod.DELETE, HttpMethod.PATCH);
+  }
+
+  @BeforeEach
+  void securitySetup() {
+    mockMvc = MockMvcBuilders.webAppContextSetup(context)
+        .apply(springSecurity()).
+        build();
   }
 
   @Test
