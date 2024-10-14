b053cbfcd08a7297af1eb0f203991803e378c174
Piotr Gliszczyński <pgliszczynski@adp.com>
2024-10-14 14:21:59 +0200

    feat/#28 - add JwtService and tests to application

---
+++ authentication-service/src/main/java/com/training/service/JwtService.java
@@ -0,0 +1,49 @@
+package com.training.service;
+
+import com.training.domain.Customer;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+@PropertySource("classpath:secure.properties")
+public class JwtService {
+
+  @Value("${security.jwt.secret-key}")
+  private String secretKey;
+  @Value("${security.jwt.expiration-time}")
+  private long jwtExpiration;
+  @Value("${auth.api.user}")
+  private String apiUser;
+
+  public String createToken(Customer customer) {
+    return Jwts
+        .builder()
+        .subject(customer.getEmail())
+        .issuedAt(new Date(System.currentTimeMillis()))
+        .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+        .signWith(getSignInKey())
+        .compact();
+  }
+
+  public String createApiToken() {
+    return Jwts
+        .builder()
+        .subject(apiUser)
+        .issuedAt(new Date(System.currentTimeMillis()))
+        .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+        .signWith(getSignInKey())
+        .compact();
+  }
+
+  private Key getSignInKey() {
+    byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+    return Keys.hmacShaKeyFor(keyBytes);
+  }
+}
