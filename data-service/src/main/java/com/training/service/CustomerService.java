45cbc092ad5ababe8bb67af20dc9631f015fe127
Piotr Gliszczyński <pgliszczynski@adp.com>
2024-10-14 12:06:13 +0200

    feat/#12 - add CustomerService implementation and CustomerService tests

---
+++ data-service/src/main/java/com/training/service/CustomerService.java
@@ -0,0 +1,58 @@
+package com.training.service;
+
+import com.training.domain.Customer;
+import com.training.exception.DuplicateElementsException;
+import com.training.exception.ElementNotFoundException;
+import com.training.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+
+  private final CustomerRepository repository;
+
+  public List<Customer> getAllCustomers(String nameFilter) {
+    List<Customer> customers = repository.findAll();
+    Optional<String> optionalNameFilter = Optional.ofNullable(nameFilter);
+    String filter = optionalNameFilter.orElse("");
+
+    return customers.stream()
+        .filter(customer -> customer.getName().contains(filter))
+        .toList();
+  }
+
+  public Customer getCustomerByEmail(String email) throws ElementNotFoundException {
+    Optional<Customer> customer = repository.findCustomerByEmail(email);
+    return customer.orElseThrow(() ->
+        new ElementNotFoundException(
+            "Could not find Customer with email " + email
+        )
+    );
+  }
+
+  public Customer createCustomer(Customer customer) throws DuplicateElementsException {
+    Customer createdCustomer = null;
+    try {
+      createdCustomer = repository.save(customer);
+    } catch (DataIntegrityViolationException e) {
+      throw new DuplicateElementsException(
+          "The customer with email " + customer.getEmail() + " already exists!");
+    }
+    return createdCustomer;
+  }
+
+  public Customer updateCustomer(Customer customer) {
+    return repository.save(customer);
+  }
+
+  public void deleteById(int id) {
+    repository.deleteById(id);
+  }
+
+}
