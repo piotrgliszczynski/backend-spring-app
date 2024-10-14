ef336890c20dbdaad3c0572aff0a51c118408c79
Piotr Gliszczyński <pgliszczynski@adp.com>
2024-10-14 18:21:03 +0200

    feat/#51 - add new Exception Handlers

--- data-service/src/main/java/com/training/config/GlobalHttpExceptionHandler.java
+++ data-service/src/main/java/com/training/config/GlobalHttpExceptionHandler.java
@@ -2,10 +2,14 @@ package com.training.config;
 
 import com.training.exception.DuplicateElementsException;
 import com.training.exception.ElementNotFoundException;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -25,8 +29,33 @@ public class GlobalHttpExceptionHandler extends ResponseEntityExceptionHandler {
     return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
   }
 
+  @ExceptionHandler(BadCredentialsException.class)
+  public ResponseEntity<Object> handleBadCredentialsException(BadCredentialsException ex) {
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+  }
+
+  @ExceptionHandler(AccessDeniedException.class)
+  public ResponseEntity<Object> handleAccessDeniedException(AccessDeniedException ex) {
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.FORBIDDEN);
+  }
+
+  @ExceptionHandler(SignatureException.class)
+  public ResponseEntity<Object> handleSignatureException(SignatureException ex) {
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+  }
+
+  @ExceptionHandler(ExpiredJwtException.class)
+  public ResponseEntity<Object> handleExpiredJwtException(ExpiredJwtException ex) {
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+  }
+
   @Override
   protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
     return new ResponseEntity<>(ex.getMessage(), status);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<Object> handleException(Exception ex) {
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
